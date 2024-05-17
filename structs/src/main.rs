3f68fd2d642b558d005743d9d578f8f38ce13e0a
Mishal Turkane <mishalturkane@gmail.com>
2024-05-17 15:52:35 +0530

    strcuts-modefied

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,10 +1,82 @@
-
 fn main() {
-    let  arr = vec![10,20,30,40,50];
-    print_arr(&arr);
+    let book = Book {
+        title: String::from("The Rusty Boy"),
+        author: String::from("Mishal"),
+        publication_year: 2029,
+
+    };
+
+
+    println!("book is {} writen by {} in {}", book.title, book.author, book.publication_year);
+    let mut book = Book {
+        title: String::from("The Rusty Boy"),
+        author: String::from("Mishal"),
+        publication_year: 2029,
+
+    };
+    book.publication_year = 2025;
+    println!("book is {} writen by {} in {}", book.title, book.author, book.publication_year);
+
+    let book_data = get_book_data(book);
+    for data in book_data {
+        println!("{data}");
+    }
+
+    let my_book = create_book("Let get rusty".to_string(),"chacha".to_string(),2024);
+    println!("and my book is {:?}",my_book);
+
+
+    let tuple_book = Tuple_book("crack the coding interview".to_string(),"mama ne likha hain".to_string(),2025);
+
+    println!("the book name is:{}",tuple_book.0);
+    println!("the author:{}",tuple_book.1);
+    println!("the publication year:{}",tuple_book.2);
+
+    let my_rectangle = Rectangle{
+        width : 20,
+        height: 20,
+    };
+
+    println!("width is :{} heigth is :{}",my_rectangle.width,my_rectangle.height);
+
+    let area = my_rectangle.area();
+    println!("are of the rectangle is:{}",area);
+
+
 }
 
-fn print_arr(arr: &Vec<i32>){
 
-    println!("{:?}",arr );
+#[derive(Debug)]
+struct Book{
+    title: String,
+    author: String,
+    publication_year: u32,
+}
+
+struct Tuple_book  (String,String,u32);
+fn get_book_data(book: Book) -> [String; 3] {
+    let title = book.title;
+    let author = book.author;
+    let publication_year = book.publication_year;
+
+    let data: [String; 3] = [title, author, publication_year.to_string()];
+    return data;
+}
+fn create_book(title: String , author: String , publication_year : u32 ) -> Book{
+        let book = Book{
+            title,
+            author,
+            publication_year,
+        };
+    return book;
+}
+
+struct  Rectangle {
+    width: u32 ,
+    height: u32 ,
+}
+impl Rectangle {
+    fn area(&self) -> u32{
+        return &self.width * &self.height;
+    }
 }
